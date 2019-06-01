500b8adcfe8351b0b4c3c3df6440e666fb84cb13
matteoxx <mateuszniziolek96@gmail.com>
2019-06-01 10:44:12 +0200

    user authentication added

--- queries.js
+++ queries.js
@@ -10,13 +10,13 @@ const pool = new Pool({
 const getCars = (request, response) => {
   pool.query("SELECT * from samochody", (err, res) => {
     if (err) {
-      console.error("error fetching data");
+      response.status(500).json({ error: "server error" });
     }
 
     console.log(res.rows);
     pool.end();
   });
-  response.send();
+  response.status(200).send();
 };
 
 const getfilteredCars = (request, response) => {
@@ -49,18 +49,42 @@ const getfilteredCars = (request, response) => {
     strToAdd = strToAdd.slice(0, strToAdd.length - 4);
   }
   query += strToAdd;
-  console.log(query);
 
+  pool.query(query, (err, res) => {
+    if (err) {
+      response.status(500).json({ error: "server error" });
+    }
+    pool.end();
+  });
+  response.status(201).json(request.body);
+};
+
+const authenticate = (request, response) => {
+  if (!request.body.login || !request.body.passwword) {
+    response.status(400).json({ error: "incorrect login or password" });
+  }
+
+  let usersData = [];
+  let query = "SELECT nazwa_uzytkownika, haslo FROM klienci";
   pool.query(query, (err, res) => {
     if (err) {
       console.error("error fetching data");
     }
+    usersData = res.data;
     pool.end();
   });
-  response.json(request.body);
+  usersData.forEach(userData => {
+    if (
+      userData.login === request.body.login &&
+      userData.passwword === request.body.passwword
+    ) {
+      response.status(200).json({ status: "OK", user: request.body.login });
+    }
+  });
 };
 
 module.exports = {
   getCars,
-  getfilteredCars
+  getfilteredCars,
+  authenticate
 };
